92d3959c65bc734004e9698d948efa11160ea340
zhangzhk <1510856038@qq.com>
2018-01-02 23:37:04 -0500

    Publish book

--- deploy.sh
+++ deploy.sh
@@ -1,2 +1,18 @@
 #!/bin/bash
 
+gitbook build
+git checkout --orphan gh-pages
+git rm --cached -r .
+git clean -df
+rm -rf *~
+
+echo "*~" > .gitignore
+echo "_book" >> .gitignore
+git add .gitignore
+git commit -m "Ignore some files"
+
+cp -r _book/* .
+git add .
+git commit -m "Publish book"
+
+git push -u origin gh-pages
